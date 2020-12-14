b6a5fd2929525b0c76109987fec4f14ae39582c5
MatarDoky <mdokydiop@gmail.com>
2020-12-14 23:29:05 +0100

    Display Checkout Data in the Django admin

--- src/main/admin.py
+++ src/main/admin.py
@@ -84,6 +84,58 @@ class ProductImageAdmin(admin.ModelAdmin):
     def product_name(self, obj):
         return obj.product.name
 
+class BasketLineInline(admin.TabularInline):
+    model = models.BasketLine
+    raw_id_fields = ("product",)
+
+@admin.register(models.Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "count")
+    list_editable = ("status",)
+    list_filter = ("status",)
+    inlines = (BasketLineInline,)
+
+class OrderLineInline(admin.TabularInline):
+    model = models.OrderLine
+    raw_id_fields = ("product",)
+
+@admin.register(models.OrderLine)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status")
+    list_editable = ("status",)
+    list_filter = ("status", "shipping_country", "date_added")
+    inlines = (OrderLineInline,)
+    fieldsets = (
+        (None, {"fields": ("user", "status")}),
+        (
+            "Billing info",
+            {
+                "fields": (
+                    "billing_name",
+                    "billing_address1",
+                    "billing_address2",
+                    "billing_zip_code",
+                    "billing_city",
+                    "billing_country",
+                )
+            },
+        ),
+        (
+            "Shipping info",
+            {
+                "fields": (
+                    "shipping_name",
+                    "shipping_address1",
+                    "shipping_address2",
+                    "shipping_zip_code",
+                    "shipping_city",
+                    "shipping_country",
+                )
+            },
+        ),
+    )
+
 admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductTag, ProductTagAdmin)
 admin.site.register(models.ProductImage, ProductImageAdmin)
