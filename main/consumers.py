5864e7bffdea44376271d3ce969e4235dc90f371
MatarDoky <mdokydiop@gmail.com>
2021-01-06 00:07:20 +0100

    Websocket Consumer

--- main/consumers.py
+++ main/consumers.py
@@ -1,5 +1,6 @@
 import aioredis
 import logging
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
--- main/routing.py
+++ main/routing.py
@@ -4,6 +4,6 @@ from . import consumers
 websocket_urlpatterns = [
     path(
         "ws/customer-service/<int:order_id>/",
-        consumers.ChatConsumer
-    )
+        consumers.ChatConsumer.as_asgi()
+    ),
 ]
