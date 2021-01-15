556c7be3075b35f5f785ee05c01a7074cf7ba5de
MatarDoky <mdokydiop@gmail.com>
2021-01-15 23:18:40 +0100

    Deploying on Dokku/Heroku fix-bug 8

--- booktime/asgi.py
+++ booktime/asgi.py
@@ -1,5 +1,4 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 from django.urls import re_path, path
