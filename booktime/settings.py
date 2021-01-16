a207fc5ca249614f94c70b4380ab4e7d98023ea5
MatarDoky <mdokydiop@gmail.com>
2021-01-16 02:19:26 +0100

    Deploying on Dokku/Heroku fix-bug 13

--- booktime/settings.py
+++ booktime/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 REST_FRAMEWORK = {
