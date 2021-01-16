f4b41def3a56c810d183cf5a0f23d7b0f40a43f6
MatarDoky <mdokydiop@gmail.com>
2021-01-16 03:57:24 +0100

    Deploying on Dokku/Heroku fix-bug 13

--- booktime/settings.py
+++ booktime/settings.py
@@ -99,7 +99,15 @@ CHANNEL_LAYERS = {
 #     }
 # }
 # else:
-DATABASES = {'default': env.db("DATABASE_URL")}
+# DATABASES = {'default': env.db("DATABASE_URL")}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
