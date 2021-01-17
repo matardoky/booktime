d0377a4a5a359728af0871590a5ddb5e3fd84350
MatarDoky <mdokydiop@gmail.com>
2021-01-17 01:41:36 +0100

    Deploying on Dokku/Heroku fix-bug

--- booktime/settings.py
+++ booktime/settings.py
@@ -15,6 +15,8 @@ env.read_env('.env')
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 ALLOWED_HOSTS = ['booktime.booktime', 'localhost']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
